618b49af3fc079a0435196efd5728a6b6eec0589
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2020-04-12 22:12:14 +0900

    初期テキストを変更

---
+++ SwiftUIChapter1/SwiftUIChapter1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIChapter1
+//
+//  Created by home on 2020/04/12.
+//  Copyright © 2020 Swift-beginners. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("ハローワールド")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
