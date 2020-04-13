e21696c5dc7062d2692f0411c97e0b95190651e1
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2020-04-13 23:42:36 +0900

    Textを追加

--- SwiftUIChapter1/SwiftUIChapter1/ContentView.swift
+++ SwiftUIChapter1/SwiftUIChapter1/ContentView.swift
@@ -10,7 +10,10 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("ハローワールド")
+        VStack {
+            Text("ハローワールド")
+            Text(/*@START_MENU_TOKEN@*/"Placeholder"/*@END_MENU_TOKEN@*/)
+        }
     }
 }
 
